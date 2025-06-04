7ada42cd7a853799494b6e4ec7f4333df7c0808a
frrld <muh.farrel.danendra@gmail.com>
2025-06-04 08:57:38 +0700

    CRUD

--- routes/web.php
+++ routes/web.php
@@ -56,12 +56,20 @@ Route::get('/linktree', function () {
 
 Route::get('dosen', [Controller::class, 'index']);
 
-Route::get('/pegawai/{nama}', [PegawaiController::class, 'show']);
-
+//formulir
 Route::get('/formulir', [PegawaiController::class, 'formulir']);
 Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
 
+//blog
 Route::get('/blog', [BlogController::class, 'home']);
 Route::get('/blog/tentang', [BlogController::class, 'tentang']);
 Route::get('/blog/kontak', [BlogController::class, 'kontak']);
 
+//CRUD
+Route::get('/pegawai', [PegawaiController::class,'index']);
+Route::get('/pegawai/tambah', [PegawaiController::class,'tambah']);
+Route::post('/pegawai/store',[PegawaiController::class,'store']);
+Route::get('/pegawai/edit/{id}', [PegawaiController::class,'edit']);
+Route::post('/pegawai/update',[PegawaiController::class,'update']);
+Route::get('/pegawai/hapus/{id}', [PegawaiController::class,'hapus']);
+// Route::get('/pegawai/{nama}', [PegawaiController::class, 'show']);
